4d0d8a1c5921b2de6fd8e5ba3e2d949ab3e4da45
zuechai <zuechai@gmail.com>
2022-10-28 10:37:55 -0400

    Add comments to users routes

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -7,10 +7,13 @@ const {
   editUser,
 } = require("../controllers/usersController");
 
+// gets all users
 router.get("/", getUsers);
 
+// gets a single user
 router.get("/:id", getSingleUser);
 
+// edits a user's properties of ticketsSubmitted, ticketsResolved, ownTicketsResolved
 router.patch("/:id", editUser);
 
 module.exports = router;
