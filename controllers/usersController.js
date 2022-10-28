724589bcbea82803b45fc804c310e27605a487ca
zuechai <zuechai@gmail.com>
2022-10-28 00:22:18 -0400

    edit json files

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,4 +8,14 @@ const getUsers = (_req, res) => {
   res.json(users);
 };
 
+// get single user
+
+// create a user
+
+// update a user's ticket counts if they were listed as a participant and/or submitted ticket (tickets or users need to track this as a property)
+
+// update a user's team and position
+
+// delete a user
+
 module.exports = { getUsers };
