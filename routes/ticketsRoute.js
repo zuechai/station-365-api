58748d8cc139d539205c58113dd4a651eac59ef8
zuechai <zuechai@gmail.com>
2022-10-28 10:34:13 -0400

    Add comments for clarity on ticket endpoints

--- routes/ticketsRoute.js
+++ routes/ticketsRoute.js
@@ -18,10 +18,10 @@ router.get("/all", getAllTickets);
 // no body required, just the id from req.params.id
 router.get("/:id", getSingleTicket);
 
-// body with all properties filled and req.params.id
+// body with all properties filled and req.params.id required
 router.put("/", createTicket);
 
-//
+// body with properties for votesFor, votesAgainst, and/or isResolved required
 router.patch("/:id", editTicket);
 
 module.exports = router;
